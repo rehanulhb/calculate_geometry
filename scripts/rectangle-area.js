e53829e4c9ff6ad3f95e23560a95918b67010cfc
rehanulhb <rehanulh@gmail.com>
2024-10-03 20:50:20 +0200

    Rectangle area design

---
+++ scripts/rectangle-area.js
@@ -0,0 +1,3 @@
+function calculaterectanglearea(){
+    console.log('Rectangle cvonnectec')
+}
--- scripts/triangle-calculate.js
+++ scripts/triangle-calculate.js
@@ -1,3 +1,24 @@
-function calcualteTriangleArea(){
-    console.log('Calculate Triangle');
+function calcualteTriangleArea() {
+    //Triangle base
+  const trianglebaseinput = document.getElementById("triangle-base");
+  const trianglebasetext = trianglebaseinput.value;
+  const base = parseFloat(trianglebasetext);
+  console.log(base);
+
+
+  //Triangle Height
+  const triangleheightinput = document.getElementById('triangle-height');
+  const triangleheighttext = triangleheightinput.value;
+  const height = parseFloat(triangleheighttext);
+  console.log(height);
+
+    const area = 0.5 * base * height;
+    console.log('Area of the triangle is :', area);
+
+
+    //Calculate Triangle Area
+
+    const triangleareaspan = document.getElementById('triangle-area');
+    triangleareaspan.innerHTML =area;
+
 }
